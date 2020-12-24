f714028b11b5c0e27ff5c5ace0e6944fd7c09aa5
Zim <zimtsui@gmail.com>
2020-12-24 21:58:17 +0800

    dev

--- dist/clone.js
+++ dist/clone.js
@@ -1,8 +1,9 @@
 import { Assets } from './data';
 import { clone as copy } from 'ramda';
+import { reviver } from './reviver';
 export function clone(x) {
     if (x instanceof Assets.AutoAssets)
-        return JSON.parse(JSON.stringify(x));
+        return JSON.parse(JSON.stringify(x), reviver);
     return copy(x);
 }
 //# sourceMappingURL=clone.js.map
--- src/clone.ts
+++ src/clone.ts
@@ -1,8 +1,9 @@
 import { Assets } from './data';
 import { clone as copy } from 'ramda';
+import { reviver } from './reviver';
 
 export function clone<T>(x: T): T {
     if (x instanceof Assets.AutoAssets)
-        return JSON.parse(JSON.stringify(x));
+        return JSON.parse(JSON.stringify(x), reviver);
     return copy(x);
 }
