043e681e080dec3d7d8c2dbc2480676f9d3df7c5
Zim <zimtsui@gmail.com>
2020-12-30 16:35:22 +0800

    dev

---
+++ dist/config.d.ts
@@ -0,0 +1,12 @@
+import Big from 'big.js';
+export interface AccountConfig {
+    leverage: number;
+    MAKER_FEE_RATE: number;
+    TAKER_FEE_RATE: number;
+}
+export interface MarketConfig {
+    PRICE_DP: number;
+    QUANTITY_DP: number;
+    CURRENCY_DP: number;
+    calcDollarVolume: (price: Big, quantity: Big) => Big;
+}
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,3 +2,4 @@ export * from './data';
 export * from './serialization';
 export * from './context';
 export * from './clone';
+export * from './config';
