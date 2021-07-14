64e493ed7d043de607b13f69176c8a53c571bbf9
Zim <zimtsui@gmail.com>
2021-07-14 08:22:13 +0800

    commonjs

---
+++ build/data.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Length = exports.Operation = exports.Side = void 0;
+var Side;
+(function (Side) {
+    Side.BID = 1;
+    Side.ASK = -1;
+})(Side = exports.Side || (exports.Side = {}));
+var Operation;
+(function (Operation) {
+    Operation.OPEN = 1;
+    Operation.CLOSE = -1;
+})(Operation = exports.Operation || (exports.Operation = {}));
+var Length;
+(function (Length) {
+    Length.LONG = 1;
+    Length.SHORT = -1;
+})(Length = exports.Length || (exports.Length = {}));
+//# sourceMappingURL=data.js.map
--- dist/index.js
+++
@@ -1,5 +0,0 @@
-export * from './data';
-export * from './serialization';
-export * from './context';
-export * from './config';
-//# sourceMappingURL=index.js.map
