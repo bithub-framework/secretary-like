4cdad5961d07d7b407968b70ea202446a47d0420
zim <zimtsui@gmail.com>
2022-02-11 14:25:24 +0800

    dev

--- build/context.d.ts
+++ build/context.d.ts
@@ -1,7 +1,8 @@
+/// <reference types="node" />
 import { LimitOrder, OpenOrder, Orderbook, Trade, Positions, Balances, Amendment } from './data';
 import { MarketSpec, AccountSpec, MarketCalc } from './specification';
 import { Timeline } from './timeline';
-import { GenericEvents, TypedEventEmitter } from 'typed-event-emitter';
+import { EventEmitter } from 'events';
 export interface ContextLike {
     [marketIndex: number]: ContextMarketLike;
     timeline: Timeline;
@@ -16,22 +17,31 @@ export interface ContextMarketLike extends ContextMarketApiLike {
 export interface ContextAccountLike extends ContextAccountApiLike {
     spec: AccountSpec;
 }
-export interface MarketEvents extends GenericEvents {
+export interface MarketEvents {
     orderbook: [Orderbook];
     trades: [Trade[]];
     error: [Error];
 }
-export declare type ContextMarketApiLike = TypedEventEmitter<MarketEvents>;
-export interface AccountEvents extends GenericEvents {
+export interface ContextMarketApiLike extends EventEmitter {
+    on<Event extends keyof MarketEvents>(event: Event, listener: (...args: MarketEvents[Event]) => void): this;
+    once<Event extends keyof MarketEvents>(event: Event, listener: (...args: MarketEvents[Event]) => void): this;
+    off<Event extends keyof MarketEvents>(event: Event, listener: (...args: MarketEvents[Event]) => void): this;
+    emit<Event extends keyof MarketEvents>(event: Event, ...args: MarketEvents[Event]): boolean;
+}
+export interface AccountEvents {
     positions: [Positions];
     balances: [Balances];
     error: [Error];
 }
-export interface ContextAccountApiLike extends TypedEventEmitter<AccountEvents> {
+export interface ContextAccountApiLike extends EventEmitter {
     makeOrders(orders: LimitOrder[]): Promise<(OpenOrder | Error)[]>;
     amendOrders(amendments: Amendment[]): Promise<(OpenOrder | Error)[]>;
     getOpenOrders(): Promise<OpenOrder[]>;
     cancelOrders(orders: OpenOrder[]): Promise<OpenOrder[]>;
     getPositions(): Promise<Positions>;
     getBalances(): Promise<Balances>;
+    on<Event extends keyof AccountEvents>(event: Event, listener: (...args: AccountEvents[Event]) => void): this;
+    once<Event extends keyof AccountEvents>(event: Event, listener: (...args: AccountEvents[Event]) => void): this;
+    off<Event extends keyof AccountEvents>(event: Event, listener: (...args: AccountEvents[Event]) => void): this;
+    emit<Event extends keyof AccountEvents>(event: Event, ...args: AccountEvents[Event]): boolean;
 }
