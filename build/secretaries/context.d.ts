4206775e1d885158ead8818fb7e592d906674910
zim <zimtsui@gmail.com>
2022-03-23 09:04:49 +0800

    dev

--- build/secretaries/context.d.ts
+++ build/secretaries/context.d.ts
@@ -11,44 +11,49 @@ import { MarketSpec, AccountSpec, MarketCalc } from './specification';
 import { Timeline } from './timeline';
 import { EventEmitter } from 'events';
 export interface ContextLike<H extends HLike<H>, OrderId, TradeId> {
-    readonly [marketIndex: number]: ContextMarketLike<H, OrderId, TradeId>;
+    readonly [marketIndex: number]: MarketLike<H, OrderId, TradeId>;
     readonly timeline: Timeline;
     /** @param value Serializable into JSON */
     submit(key: string, value: any): Promise<void>;
 }
-export interface ContextMarketLike<H extends HLike<H>, OrderId, TradeId> extends ContextMarketApiLike<H, TradeId> {
-    readonly [accountIndex: number]: ContextAccountLike<H, OrderId>;
+export interface MarketLike<H extends HLike<H>, OrderId, TradeId> extends MarketApiLike<H, OrderId, TradeId> {
+    readonly [accountIndex: number]: AccountLike<H, OrderId, TradeId>;
     readonly spec: MarketSpec<H>;
     readonly calc: MarketCalc<H>;
 }
-export interface ContextAccountLike<H extends HLike<H>, OrderId> extends ContextAccountApiLike<H, OrderId> {
+export interface AccountLike<H extends HLike<H>, OrderId, TradeId> extends AccountApiLike<H, OrderId, TradeId> {
     readonly spec: AccountSpec;
+    readonly events: AccountEventsLike<H, OrderId, TradeId>;
 }
-export interface MarketEvents<H extends HLike<H>, TradeId> {
+export interface MarketEvents<H extends HLike<H>, OrderId, TradeId> {
     orderbook: [Orderbook.MutablePlain<H>];
     trades: [Trade.MutablePlain<H, TradeId>[]];
     error: [Error];
 }
-export interface ContextMarketApiLike<H extends HLike<H>, TradeId> extends EventEmitter {
-    on<Event extends keyof MarketEvents<H, TradeId>>(event: Event, listener: (...args: MarketEvents<H, TradeId>[Event]) => void): this;
-    once<Event extends keyof MarketEvents<H, TradeId>>(event: Event, listener: (...args: MarketEvents<H, TradeId>[Event]) => void): this;
-    off<Event extends keyof MarketEvents<H, TradeId>>(event: Event, listener: (...args: MarketEvents<H, TradeId>[Event]) => void): this;
-    emit<Event extends keyof MarketEvents<H, TradeId>>(event: Event, ...args: MarketEvents<H, TradeId>[Event]): boolean;
+export interface MarketApiLike<H extends HLike<H>, OrderId, TradeId> {
 }
-export interface AccountEvents<H extends HLike<H>> {
+export interface MarketEventsLike<H extends HLike<H>, OrderId, TradeId> extends EventEmitter {
+    on<Event extends keyof MarketEvents<H, OrderId, TradeId>>(event: Event, listener: (...args: MarketEvents<H, OrderId, TradeId>[Event]) => void): this;
+    once<Event extends keyof MarketEvents<H, OrderId, TradeId>>(event: Event, listener: (...args: MarketEvents<H, OrderId, TradeId>[Event]) => void): this;
+    off<Event extends keyof MarketEvents<H, OrderId, TradeId>>(event: Event, listener: (...args: MarketEvents<H, OrderId, TradeId>[Event]) => void): this;
+    emit<Event extends keyof MarketEvents<H, OrderId, TradeId>>(event: Event, ...args: MarketEvents<H, OrderId, TradeId>[Event]): boolean;
+}
+export interface AccountEvents<H extends HLike<H>, OrderId, TradeId> {
     positions: [Positions.MutablePlain<H>];
     balances: [Balances.MutablePlain<H>];
     error: [Error];
 }
-export interface ContextAccountApiLike<H extends HLike<H>, OrderId> extends EventEmitter {
+export interface AccountApiLike<H extends HLike<H>, OrderId, TradeId> {
     makeOrders(orders: readonly LimitOrder<H>[]): Promise<(OpenOrder.MutablePlain<H, OrderId> | Error)[]>;
     amendOrders(amendments: readonly Amendment<H, OrderId>[]): Promise<(OpenOrder.MutablePlain<H, OrderId> | Error)[]>;
     getOpenOrders(): Promise<OpenOrder.MutablePlain<H, OrderId>[]>;
     cancelOrders(orders: readonly OpenOrder<H, OrderId>[]): Promise<OpenOrder.MutablePlain<H, OrderId>[]>;
     getPositions(): Promise<Positions.MutablePlain<H>>;
     getBalances(): Promise<Balances.MutablePlain<H>>;
-    on<Event extends keyof AccountEvents<H>>(event: Event, listener: (...args: AccountEvents<H>[Event]) => void): this;
-    once<Event extends keyof AccountEvents<H>>(event: Event, listener: (...args: AccountEvents<H>[Event]) => void): this;
-    off<Event extends keyof AccountEvents<H>>(event: Event, listener: (...args: AccountEvents<H>[Event]) => void): this;
-    emit<Event extends keyof AccountEvents<H>>(event: Event, ...args: AccountEvents<H>[Event]): boolean;
+}
+export interface AccountEventsLike<H extends HLike<H>, OrderId, TradeId> extends EventEmitter {
+    on<Event extends keyof AccountEvents<H, OrderId, TradeId>>(event: Event, listener: (...args: AccountEvents<H, OrderId, TradeId>[Event]) => void): this;
+    once<Event extends keyof AccountEvents<H, OrderId, TradeId>>(event: Event, listener: (...args: AccountEvents<H, OrderId, TradeId>[Event]) => void): this;
+    off<Event extends keyof AccountEvents<H, OrderId, TradeId>>(event: Event, listener: (...args: AccountEvents<H, OrderId, TradeId>[Event]) => void): this;
+    emit<Event extends keyof AccountEvents<H, OrderId, TradeId>>(event: Event, ...args: AccountEvents<H, OrderId, TradeId>[Event]): boolean;
 }
