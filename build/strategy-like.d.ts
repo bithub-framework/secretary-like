491427895d04632237422ada2b7417b91cbdea81
Zim <zimtsui@gmail.com>
2022-07-27 15:05:22 +0800

    strategyStatic

--- build/strategy-like.d.ts
+++ build/strategy-like.d.ts
@@ -1,3 +1,8 @@
 import { StartableLike } from 'startable';
+import { ContextLike } from './context-like';
+import { HLike } from './data/h';
 export interface StrategyLike extends StartableLike {
+}
+export interface StrategyStaticLike<H extends HLike<H>> {
+    new (ctx: ContextLike<H>): StrategyLike;
 }
--- src/strategy-like.ts
+++ src/strategy-like.ts
@@ -1,4 +1,9 @@
 import { StartableLike } from 'startable';
+import { ContextLike } from './context-like';
+import { HLike } from './data/h';
 
 
 export interface StrategyLike extends StartableLike { }
+export interface StrategyStaticLike<H extends HLike<H>> {
+	new(ctx: ContextLike<H>): StrategyLike;
+}
