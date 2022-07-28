1aa7989974f31d92c6ccf3d3b8cf00f834cd50e8
Zim <zimtsui@gmail.com>
2022-07-28 21:19:24 +0800

    datatypes

---
+++ build/data/data-types.d.ts
@@ -0,0 +1,24 @@
+import { HLike, HFactory, HStatic } from './h';
+import { LimitOrderFactory } from './limit-order';
+import { OpenOrderFactory } from './open-order';
+import { AmendmentFactory } from './amendment';
+import { OrderbookFactory } from './orderbook';
+import { TradeFactory } from './trade';
+import { PositionFactory } from './position';
+import { PositionsFactory } from './positions';
+import { BookOrderFactory } from './book-order';
+import { BalancesFactory } from './balances';
+export declare class DataTypesNamespace<H extends HLike<H>> {
+    hFactory: HFactory<H>;
+    H: HStatic<H>;
+    constructor(hFactory: HFactory<H>, H: HStatic<H>);
+    limitOrderFactory: LimitOrderFactory<H>;
+    openOrderFactory: OpenOrderFactory<H>;
+    amendmentFactory: AmendmentFactory<H>;
+    bookOrderFactory: BookOrderFactory<H>;
+    orderbookFactory: OrderbookFactory<H>;
+    tradeFactory: TradeFactory<H>;
+    balancesFactory: BalancesFactory<H>;
+    positionFactory: PositionFactory<H>;
+    positionsFactory: PositionsFactory<H>;
+}
