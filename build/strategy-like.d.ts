47e43beede5f16c07ce8fca172d5421fbe58b299
Zim <zimtsui@gmail.com>
2022-08-11 00:29:23 +0800

    comments

--- build/strategy-like.d.ts
+++ build/strategy-like.d.ts
@@ -1,6 +1,10 @@
 import { Startable } from 'startable';
 import { ContextLike } from './context-like';
 import { HLike } from './data-types/h';
+/**
+ * `ctx` is startable and is aggregated by strategy via assart().
+ * Therefore, strategy shouldn't use ctx during `STARTING`.
+ */
 export interface StrategyLike {
     $s: Startable;
 }
--- src/strategy-like.ts
+++ src/strategy-like.ts
@@ -3,6 +3,10 @@ import { ContextLike } from './context-like';
 import { HLike } from './data-types/h';
 
 
+/**
+ * `ctx` is startable and is aggregated by strategy via assart().
+ * Therefore, strategy shouldn't use ctx during `STARTING`.
+ */
 export interface StrategyLike {
 	$s: Startable;
 }
